e096fb0575cff059eaad7e75efb9a0d7467497fe
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-07 16:51:14 +0200

    Configure Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-root "users#index"
+  devise_for :users
+ 
+
+  root "foods#index"
 
   resources :foods
-  resources :user do
-    resources :recipes  
-  
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
   end
 
+  
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
