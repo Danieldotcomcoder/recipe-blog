f8406528b1376f08f89865c004884486fe6f9708
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-08 10:34:09 +0200

    Add and show Recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,27 +1,23 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[ show edit update destroy ]
 
-  # GET /recipes or /recipes.json
+  
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
-  # GET /recipes/1 or /recipes/1.json
   def show
   end
 
-  # GET /recipes/new
   def new
     @recipe = Recipe.new
   end
 
-  # GET /recipes/1/edit
   def edit
   end
 
-  # POST /recipes or /recipes.json
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.new(recipe_params)
 
     respond_to do |format|
       if @recipe.save
@@ -34,7 +30,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
@@ -47,7 +42,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy
 
@@ -63,7 +57,7 @@ class RecipesController < ApplicationController
       @recipe = Recipe.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    
     def recipe_params
       params.require(:recipe).permit(:name, :preparationTime, :cookingTime, :description, :public)
     end
