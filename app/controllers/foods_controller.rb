254373592a9b501f38cda9403a13d8e7ffbaf8a0
Somdotta07 <jhilick.s@gmail.com>
2022-03-08 09:57:06 +0100

    Update foods_controller.rb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -23,10 +23,8 @@ class FoodsController < ApplicationController
     respond_to do |format|
       if @food.save
         format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
-        format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
   end
