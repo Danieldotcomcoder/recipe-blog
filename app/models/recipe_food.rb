0d914f0a069d6a80b6c44eada74ad70d919fffd5
Somdotta07 <jhilick.s@gmail.com>
2022-03-08 14:48:17 +0100

    Add method and display

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,2 +1,8 @@
 class RecipeFood < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :food
+
+  def cal_value(quantity, price)
+    quantity * price
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'public_recipes/index'
   devise_for :users
  
 
@@ -9,7 +8,7 @@ Rails.application.routes.draw do
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
   end
-
+  resources :public_recipes, only: [:index]
   
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy' 
